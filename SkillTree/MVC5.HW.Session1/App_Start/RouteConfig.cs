783a594f4e868f93bce3edb0e937f9f163bce7b2
imchengr <imchengr@gmail.com>
2016-07-31 01:02:16 +0800

    1.add controller 2.add view with the layout template

--- SkillTree/MVC5.HW.Session1/App_Start/RouteConfig.cs
+++ SkillTree/MVC5.HW.Session1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC5.HW.Session1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Accounting", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
